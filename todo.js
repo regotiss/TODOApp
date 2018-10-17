09f750fd6988e58a179f7d0e66a140ad63f6775b
Sujata Regoti <sujatar@thoughtworks.com>
2018-10-17 19:41:43 +0530

    Add onclick listener to new task item

--- todo.js
+++ todo.js
@@ -4,12 +4,18 @@ const addToDo = () => {
     
     const newTaskItem = document.createElement('li');
     newTaskItem.innerText = newTask.value;
-    
+    newTaskItem.onclick = removeFromList.bind(this, task.length);
+        
     const todoList = document.getElementById('toDoList');
     todoList.appendChild(newTaskItem);
-    task.add(newTask);
+    tasks.push(newTask);
     
     newTask.value = '';
+}
+
+const removeFromList = (index) => {
+    tasks.splice(index, 1);
+    console.log(index);
 }
 
 
