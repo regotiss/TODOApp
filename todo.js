cf3f02a931ab612e5d2bdef6fef7d8b9288f3544
Sujata Regoti <sujatar@thoughtworks.com>
2018-10-23 22:23:28 +0530

    Remove to do item on click of item

--- todo.js
+++ todo.js
@@ -1,21 +1,31 @@
 let tasks = [];
 const addToDo = () => {
-    const newTask = document.getElementById('newTask');
-    
-    const newTaskItem = document.createElement('li');
-    newTaskItem.innerText = newTask.value;
-    newTaskItem.onclick = removeFromList.bind(this, task.length);
-        
+    const newTask = document.getElementById('newTask');    
     const todoList = document.getElementById('toDoList');
-    todoList.appendChild(newTaskItem);
-    tasks.push(newTask);
+    todoList.appendChild(newToDoItem(newTask.value, tasks.length));
+    
+    tasks.push(newTask.value);
     
     newTask.value = '';
 }
 
 const removeFromList = (index) => {
     tasks.splice(index, 1);
-    console.log(index);
+    const todoList = document.getElementById('toDoList');
+    todoList.innerText = '';
+    constructToDo();
+}
+
+const constructToDo = () => {
+    const todoList = document.getElementById('toDoList');
+    tasks.map((task, index) => todoList.appendChild(newToDoItem(task, index)));
+}
+
+const newToDoItem = (task, index) => {
+    const toDoItem = document.createElement('li');
+    toDoItem.innerText = task;
+    toDoItem.onclick = removeFromList.bind(this, index);
+    return toDoItem;
 }
 
 
